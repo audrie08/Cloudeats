229242b23735a68a293c874889f9b63f71297f9d
audrie08 <olonaaudreyy@gmail.com>
2025-08-27 17:13:53 +0800

    Update prodsched.py

--- prodsched.py
+++ prodsched.py
@@ -22,7 +22,7 @@ warnings.filterwarnings('ignore')
 # --- PAGE CONFIG ---
 st.set_page_config(
     page_title="Commissary Production Scheduler",
-    layout="wide",
+    layout="centered",
     initial_sidebar_state="collapsed"
 )
 
