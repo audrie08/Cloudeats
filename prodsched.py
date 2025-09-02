b884a3788f3c9bdc60ea98daf367766a8852bcf7
audrie08 <olonaaudreyy@gmail.com>
2025-09-02 11:06:03 +0800

    Update prodsched.py

--- prodsched.py
+++ prodsched.py
@@ -3463,15 +3463,15 @@ def summary_page():
                 
             # Week options (customize based on your needs)
             week_options = [
-                "Week 1", "Week 2", "Week 3", "Week 4", "Week 5", "Week 6",
-                "Week 7", "Week 8", "Week 9", "Week 10", "Week 11", "Week 12",
-                "Week 13", "Week 14", "Week 15", "Week 16", "Week 17", "Week 18",
-                "Week 19", "Week 20", "Week 21", "Week 22", "Week 23", "Week 24",
-                "Week 25", "Week 26", "Week 27", "Week 28", "Week 29", "Week 30",
-                "Week 31", "Week 32", "Week 33", "Week 34", "Week 35", "Week 36",
-                "Week 37", "Week 38", "Week 39", "Week 40", "Week 41", "Week 42",
-                "Week 43", "Week 44", "Week 45", "Week 46", "Week 47", "Week 48",
-                "Week 49", "Week 50", "Week 51", "Week 52", "Week 53"
+                "1", "2", "3", "4", "5", "6",
+                "7", "8", "9", "10", "11", "12",
+                "13", "14", "15", "16", "17", "18",
+                "19", "20", "21", "22", "23", "24",
+                "25", "26", "27", "28", "29", "30",
+                "31", "32", "33", "34", "35", "36",
+                "37", "38", "39", "40", "41", "42",
+                "43", "44", "45", "46", "47", "48",
+                "49", "50", "51", "52", "53"
             ]
             
             # Streamlit week selector
