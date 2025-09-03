6ac05950073c50e13e2c36eb0481eaf20b48a77f
audrie08 <olonaaudreyy@gmail.com>
2025-09-03 10:13:27 +0800

    Update prodsched.py

--- prodsched.py
+++ prodsched.py
@@ -3492,7 +3492,7 @@ def summary_page():
         selected_week = st.selectbox(
             "Select Week:",
             options=list(range(1, 54)),  # Weeks 1-53
-            index=None
+            index=None,
             key="week_selector"
         )
     
